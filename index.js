ede3eebb59ee47de06bf57d5f8f974870df73ef7
Michael Davis <micdavis93@gmail.com>
2022-11-14 11:59:24 -0700

    created some DOM variables, removed HTML radio

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Create global variables
+// Character Info
+const charImg = document.querySelector("#character-image")
+const charRating = document.querySelector("#character-rating")
+const charName = document.querySelector("#character-name")
+const charStatus = document.querySelector("#character-status")
+const charOrigin = document.querySelector("#character-origin")
+const charFirstApp = document.querySelector("#character-first-appearance")
 
+const charImg = document.querySelector("#comments-form")
 
 // Get Request
 {
@@ -13,12 +21,31 @@ fetch("https://rickandmortyapi.com/api/characters/result")
 }
 
 // Declare and define functions
-function listCharacters(characters){
+function listCharacters(character){
+    forEach(character => {
 
+    })
+}
+
+function listCharacters(charData){
+    const charBar = document.querySelector("#character-bar")
+    
+    charData.forEach(character => {
+        const span = document.createElement("span")
+        span.innerText = character.name
+        charBar.append(span)
+
+        // 2. SHOW DETAILS function on click (should this go before append?)
+        span.addEventListener("click", (e) => {
+            showDetails(character)
+        })
+    })
 }
 
 function showInformation(current){
 
+
+    submitComment(current)
 }
 
 function submitComment(current){
