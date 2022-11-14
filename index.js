809409ef566bd32a6945255739b5c5237de96113
Michael Davis <micdavis93@gmail.com>
2022-11-14 13:27:59 -0700

    edited HTML, added images, updated functions

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ const charName = document.querySelector("#character-name")
 const charStatus = document.querySelector("#character-status")
 const charOrigin = document.querySelector("#character-origin")
 const charFirstApp = document.querySelector("#character-first-appearance")
-
+const jerryApproved = document.querySelector("#jerry-approved")
+const jerryDisapproved = document.querySelector("#jerry-disapproved")
 
 const commentsForm = document.querySelector("#comments-form")
 const commentsList = document.querySelector("#comments-list")
 
 // Get Request
-fetch("https://rickandmortyapi.com/api/characters/result")
+fetch("https://rickandmortyapi.com/api/characters/results")
     .then(r => r.json())
     .then(characters => {
         listCharacters(characters)
@@ -26,9 +27,9 @@ fetch("https://rickandmortyapi.com/api/characters/result")
 
 function listCharacters(character){
     forEach(character => {
-        const nav = document.createElement("nav");
-        nav.innerText = character.name;
-        charNameList.append(nav);
+        const h5 = document.createElement("h5");
+        h5.innerText = character.name;
+        charNameList.append(h5);
 
         nav.addEventListener("click", (e) => {
             showInformation(current)
@@ -36,26 +37,33 @@ function listCharacters(character){
     });
 };
 
-// function listCharacters(charData){
-//     const charBar = document.querySelector("#character-bar")
-    
-//     charData.forEach(character => {
-//         const span = document.createElement("span")
-//         span.innerText = character.name
-//         charBar.append(span)
-
-//         // 2. SHOW DETAILS function on click (should this go before append?)
-//         span.addEventListener("click", (e) => {
-//             showDetails(character)
-//         })
-//     })
-// }
-
 function showInformation(current){
+    charImg.src = current.image
+    charName.innerText = current.name
 
 
     submitComment(current)
 }
+
+function jerryRating(current){
+        ticketButton.addEventListener("click", ()=>{
+        tixBought.innerText = `${current.tickets_bought ++} Tickets Bought``
+}
+
+// 2. SHOW DETAILS function
+// function showDetails(current){
+//     charName.innerText = current.name
+//     charImg.src = current.image
+//     voteCount.innerText = current.votes
+
+//     voteSubmit(current)
+// }
+
+// function buyTicket(){
+//     ticketButton.addEventListener("click", ()=>{
+//         tixBought.innerText = `${current.tickets_bought ++} Tickets Bought`
+//     })
+// }
 
 function submitComment(current){
 
