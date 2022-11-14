5bcac232e71aac1e41dce71b2177f1d5288693e5
Michael Davis <micdavis93@gmail.com>
2022-11-14 12:03:48 -0700

    delete radio, finish declaring JS variables

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const charStatus = document.querySelector("#character-status")
 const charOrigin = document.querySelector("#character-origin")
 const charFirstApp = document.querySelector("#character-first-appearance")
 
-const charImg = document.querySelector("#comments-form")
+
+const commentsForm = document.querySelector("#comments-form")
+const commentsList = document.querySelector("#comments-list")
 
 // Get Request
 {
@@ -27,20 +29,20 @@ function listCharacters(character){
     })
 }
 
-function listCharacters(charData){
-    const charBar = document.querySelector("#character-bar")
+// function listCharacters(charData){
+//     const charBar = document.querySelector("#character-bar")
     
-    charData.forEach(character => {
-        const span = document.createElement("span")
-        span.innerText = character.name
-        charBar.append(span)
+//     charData.forEach(character => {
+//         const span = document.createElement("span")
+//         span.innerText = character.name
+//         charBar.append(span)
 
-        // 2. SHOW DETAILS function on click (should this go before append?)
-        span.addEventListener("click", (e) => {
-            showDetails(character)
-        })
-    })
-}
+//         // 2. SHOW DETAILS function on click (should this go before append?)
+//         span.addEventListener("click", (e) => {
+//             showDetails(character)
+//         })
+//     })
+// }
 
 function showInformation(current){
 
